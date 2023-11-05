6a22acb0cbc82748ef1263acabc725e645534bfb
s1lencebtw <Vitalii.Letnianchyn.RI.2023@lpnu.ua>
2023-11-05 23:44:36 +0200

    Add project files.

---
+++ Lab 6.3 rec/Lab 6.3 rec.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+
+void printArray(int* array, int size, int i);
+void generateArray(int* arr, int size, int min, int max, int i);
+int CountElements(const int* arr, int size, int count, int i);
+template <typename T>
+void printArray1(T* array, int size, int i) {
+    cout << setw(4) << array[i] << " ";
+    if (i < size - 1)
+        printArray(array, size, i + 1);
+    else cout << endl;
+}
+
+template <typename T>
+void generateArray1(T* arr, int size, int min, int max, int i) {
+    if (i < size) {
+        arr[i] = min + rand() % (max - min + 1);
+        generateArray(arr, size, min, max, i + 1);
+    }
+    else return;
+}
+
+template <typename T>
+T CountElements1(const T* arr, int size, T count, int i) {
+    if (i < size) {
+        if (arr[i] % 2 != 0) {
+            return CountElements1(arr, size, count + 1, i + 1);
+        }
+        else {
+            return CountElements1(arr, size, count, i + 1);
+        }
+    }
+    return count;
+}
+
+
+
+int main() {
+    srand(unsigned(time(NULL)));
+    const int arrSize = 20;
+    int a[arrSize];
+
+    generateArray(a, arrSize, -50, 50, 0);
+
+    cout << "Array Elements:                       ";
+    printArray(a, arrSize, 0);
+
+    int CountNumbers = CountElements(a, 20, 0, 0);
+    cout << "Count of elements which not divide 2: " << CountNumbers << endl;
+
+    const int arrSize1 = 20;
+    int b[arrSize1];
+
+    generateArray1(b, arrSize1, -50, 50, 0);
+    cout << "Array Elements:                       ";
+    printArray1(b, arrSize1, 0);
+
+    int CountNumbers1 = CountElements1(a, 20, 0, 0);
+    cout << "Count of elements which not divide 2: " << CountNumbers1 << endl;
+    return 0;
+}
+
+
+
+void printArray(int* array, int size, int i) {
+    cout << setw(4) << array[i] << " ";
+    if (i < size - 1)
+        printArray(array, size, i + 1);
+    if (i == size - 1)
+        cout << endl;
+}
+
+
+
+void generateArray(int* arr, int size, int min, int max, int i) {
+    if (i < size) {
+        arr[i] = min + rand() % (max - min + 1);
+        generateArray(arr, size, min, max, i + 1);
+    }
+    else return;
+}
+
+int CountElements(const int* arr, int size, int count, int i) {
+
+    if (i <= size) {
+        if (arr[i] % 2 != 0) {
+            return CountElements(arr, size, count + 1, i + 1);
+        }
+        else return CountElements(arr, size, count, i + 1);
+        
+    }
+
+    return count;
+}
+
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 6.3 rec/Lab 6.3 rec.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int a[20] = { 13, 15, -5, -9, -11, -45, 48, 40, 7, 24, 32, 13, 37, 47, -42, 36, -13, -36, 6, 17 };
+			int c = CountElements(a, 20, 0, 0);
+			Assert::AreEqual(c, 12);
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			int a[20] = { 13, 15, -5, -9, -11, -45, 48, 40, 7, 24, 32, 13, 37, 47, -42, 36, -13, -36, 6, 17 };
+			int c = CountElements1(a, 20, 0, 0);
+			Assert::AreEqual(c, 12);
+		}
+	};
+}
